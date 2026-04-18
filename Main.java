4a35c166cf6ec65401dff305f6fdf1c58b29a35d
dfbtiv <dfebriyanti204@gmail.com>
2026-04-18 21:09:52 +0700

    sec commit

--- Main.java
+++ Main.java
@@ -1,18 +1,64 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         TodoManager manager = new TodoManager();
+        Scanner input = new Scanner(System.in);
+        boolean isRunning = true; /* Tombol on off */
 
-        manager.addTask("Belajar Java");
-        manager.addTask("Daftar Magang");
+        System.out.println("Welcome di To Do App dfbtiv!");
 
-        manager.viewTasks();
+        // loop: aplikasi akan terus berputar selama isRunning = true
+        while (isRunning) {
+            System.out.println("\n MENU TO DO APP");
+            System.out.println("1. Tambah Task Baru");
+            System.out.println("2. Lihat Semua Task");
+            System.out.println("3. Selesaikan Tugas");
+            System.out.println("4. Exit");
+            System.out.print("Pilih Menu (1-4: ");
 
-        System.out.println("\n------------------");
+            int pilihan = input.nextInt();
+            input.nextLine(); /* utk membersihkan enter */
 
-        manager.markTaskAsDone(1);
+            if (pilihan == 1) {
+                System.out.println("Ketik tugas yang mau ditambahkan: ");
+                String namaTugas = input.nextLine();
+                manager.addTask(namaTugas);
+            }
 
-        System.out.println("------------------");
+            else if (pilihan == 2) {
+                manager.viewTasks();
+            }
 
-        manager.viewTasks();
+            else if (pilihan == 3) {
+                manager.viewTasks();
+                System.out.print("Masukan nomor tugas yang sudah selesai: ");
+                int nomor = input.nextInt();
+                manager.markTaskAsDone(nomor - 1);
+            }
+
+            else if (pilihan == 4) {
+                System.out.println("Terimakasih sudah menggunakan aplikasi ini. See You!");
+                isRunning = false; /* matikan loop */
+            }
+
+            else {
+                System.out.println("Pilihan Tidak Valid, coba lagi!");
+            }
+        }
+        input.close(); /* matikan scanner agar hemat memori */
+
+        // manager.addTask("Belajar Java");
+        // manager.addTask("Daftar Magang");
+
+        // manager.viewTasks();
+
+        // System.out.println("\n------------------");
+
+        // manager.markTaskAsDone(1);
+
+        // System.out.println("------------------");
+
+        // manager.viewTasks();
     }
 }
